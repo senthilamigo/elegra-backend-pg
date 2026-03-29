1aaf4f1545b8ad79138f15d7563cfc7bfe93b3aa
SenthilKumar <37953332+senthilamigo@users.noreply.github.com>
2026-03-29 12:42:28 +0530

    Create order_details table for order management

---
+++ sql/order_details.sql
@@ -0,0 +1,10 @@
+create table public.order_details (
+  id uuid not null default extensions.uuid_generate_v4 (),
+  order_id uuid not null,
+  product_id uuid not null,
+  quantity integer not null,
+  unit_price numeric(12, 2) not null,
+  constraint order_details_pkey primary key (id),
+  constraint fk_order_details_product_variant foreign KEY (product_id) references product_variants (id) on delete RESTRICT,
+  constraint order_details_order_id_fkey foreign KEY (order_id) references orders (id) on delete CASCADE
+) TABLESPACE pg_default;
